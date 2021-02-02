532113eb2ec47274edb68647f27481e246cfae68
Terence Ateya <ateyaterence@gmail.com>
2021-02-02 09:44:50 -0600

    first commit

---
+++ model/Student.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String studntID;
+	private String courses = " ";
+	private double tuitionBalance;
+	private static int costPerCourse = 600;
+	private static int id = 1000;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+
+
+
+	
+}
---
+++ test/StudentDatabaseAppTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class StudentDatabaseAppTest {
+	public static void main(String[] args){
+		
+	}
+	
+}
