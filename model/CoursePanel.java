3ff151982b69301055fcea6dfeb4037314048bb4
Terence Ateya <ateyaterence@gmail.com>
2021-02-03 01:37:31 -0600

    next update

---
+++ model/CoursePanel.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.swing.JFrame;
+
+public class CoursePanel {
+	private JFrame window;
+
+	public CoursePanel(JFrame window){
+		this.window = window;
+	}
+	
+	
+}
---
+++ view/MenuScreen.java
@@ -0,0 +1,40 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+
+public class MenuScreen {
+	private JFrame window;
+
+	public MenuScreen(JFrame window){
+		this.window = window;
+	}
+
+	public void init(){
+		Container cp = window.getContentPane();
+
+		JPanel menuPanel = new JPanel();
+		menuPanel.setPreferredSize(new Dimension(400, 200));
+
+		JButton courseButton = new JButton("CHECK AND ENROLL COURSES");
+		menuPanel.add(courseButton);
+
+		cp.add(BorderLayout.CENTER, menuPanel);
+
+		courseButton.addActionListener(event->{
+			window.getContentPane().removeAll();
+			var panel = new CoursePanel(window);
+			panel.init();
+			window.pack();
+			window.revalidate();
+		});
+	}
+
+	
+}
